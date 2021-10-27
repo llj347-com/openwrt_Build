6c876969fd5dedffdddf95f8ec6dde2b1edd86cc
llj347-com <82518711+llj347-com@users.noreply.github.com>
2021-10-27 22:02:29 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,3 +10,8 @@ sed -i 's/192.168.1.1/192.168.1.252/g' openwrt/package/base-files/files/bin/conf
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/kenzok8/luci-theme-ifit.git package/lean/luci-theme-ifit
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
